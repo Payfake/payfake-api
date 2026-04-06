a76cf22bed9439c1422468db1e98cc0ac6a04f23
GordenArcher <archergorden@gmail.com>
2026-04-06 00:16:34 +0000

    feat: add config and database connection - typed Config struct with App, Database and JWT sub-configs - godenv.MustLoad for bootstrapping with validation on startup - DSN built from env with Africa/Accra timezone baked in - GORM connection with environment-aware query logging - AutoMigrate wired up ready for domain models

---
+++ internal/config/config.go
@@ -0,0 +1,86 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/GordenArcher/godenv"
+)
+
+type Config struct {
+	App      AppConfig
+	Database DatabaseConfig
+	JWT      JWTConfig
+}
+
+type AppConfig struct {
+	Name        string
+	Env         string
+	Port        string
+	FrontendURL string
+}
+
+type DatabaseConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Name     string
+	SSLMode  string
+	DSN      string
+}
+
+type JWTConfig struct {
+	Secret      string
+	ExpiryHours string
+}
+
+func Load() (*Config, error) {
+	godenv.MustLoad()
+
+	cfg := &Config{
+		App: AppConfig{
+			Name:        godenv.Get("APP_NAME", "payfake"),
+			Env:         godenv.Get("APP_ENV", "development"),
+			Port:        godenv.Get("APP_PORT", "8080"),
+			FrontendURL: godenv.Get("FRONTEND_URL", "http://localhost:5173"),
+		},
+		Database: DatabaseConfig{
+			Host:     godenv.Get("DB_HOST", "localhost"),
+			Port:     godenv.Get("DB_PORT", "5432"),
+			User:     godenv.Get("DB_USER", "postgres"),
+			Password: godenv.Get("DB_PASSWORD", ""),
+			Name:     godenv.Get("DB_NAME", "payfake"),
+			SSLMode:  godenv.Get("DB_SSLMODE", "disable"),
+		},
+		JWT: JWTConfig{
+			Secret:      godenv.Get("JWT_SECRET", ""),
+			ExpiryHours: godenv.Get("JWT_EXPIRY_HOURS", "24"),
+		},
+	}
+
+	cfg.Database.DSN = fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s TimeZone=Africa/Accra",
+		cfg.Database.Host,
+		cfg.Database.Port,
+		cfg.Database.User,
+		cfg.Database.Password,
+		cfg.Database.Name,
+		cfg.Database.SSLMode,
+	)
+
+	if err := cfg.validate(); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
+func (c *Config) validate() error {
+	if c.JWT.Secret == "" {
+		return fmt.Errorf("JWT_SECRET is required")
+	}
+	if c.Database.Password == "" {
+		return fmt.Errorf("DB_PASSWORD is required")
+	}
+	return nil
+}
---
+++ internal/database/database.go
@@ -0,0 +1,61 @@
+package database
+
+import (
+	"log"
+
+	"github.com/GordenArcher/payfake/internal/config"
+	"github.com/GordenArcher/payfake/internal/domain"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type Database struct {
+	DB *gorm.DB
+}
+
+func Connect(cfg *config.Config) (*Database, error) {
+	gormCfg := &gorm.Config{}
+
+	if cfg.App.Env == "development" {
+		gormCfg.Logger = logger.Default.LogMode(logger.Info)
+	} else {
+		gormCfg.Logger = logger.Default.LogMode(logger.Silent)
+	}
+
+	db, err := gorm.Open(postgres.Open(cfg.Database.DSN), gormCfg)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("[payfake] database connected")
+
+	if err := migrate(db); err != nil {
+		return nil, err
+	}
+
+	return &Database{DB: db}, nil
+}
+
+func migrate(db *gorm.DB) error {
+	log.Println("[payfake] running migrations...")
+
+	err := db.AutoMigrate(
+		&domain.Merchant{},
+		&domain.Customer{},
+		&domain.Transaction{},
+		&domain.Charge{},
+		&domain.WebhookEndpoint{},
+		&domain.WebhookEvent{},
+		&domain.WebhookAttempt{},
+		&domain.ScenarioConfig{},
+		&domain.RequestLog{},
+	)
+
+	if err != nil {
+		return err
+	}
+
+	log.Println("[payfake] migrations complete")
+	return nil
+}
